c04d686fe1c57fac480f380c6af23f6cb61a3fc4
APUiscool <85752984+APUiscool@users.noreply.github.com>
2022-07-16 18:02:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -30,8 +30,6 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appi
 
 
 })
-
-.catch(err => alert("Something Went Wrong: city doesn't exist"))
 }
 
 function DefaultScreen(){
